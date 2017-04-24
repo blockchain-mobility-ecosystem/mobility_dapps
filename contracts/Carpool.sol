c29d13af1f4301e8952da19840d4bc212df62435
lexon <co.liang.ol@gmail.com>
2017-04-24 12:02:19 -0400

    Corrected syntax to pass truffile compile

--- contracts/Carpool.sol
+++ contracts/Carpool.sol
@@ -1,19 +1,19 @@
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.8;
 
 //import "CoMotionToken.sol"
-import "./CarpoolTypes.sol"
+import "./CarpoolTypes.sol";
 
 /**
  * Carpool contract acts as carpool business layer.
  */
-contract Carpool {
+contract Carpool is CarpoolTypes {
     enum Color { WHITE, BLACK, BLUE, RED}
     enum Transmission {MANUAL, AUTOMATIC}
     enum CarStatus {AVAILABLE, UNAVAILABLE}
 
     struct Car {
-        carid_t id;
-        vin_t vin;
+        uint32 id;
+        bytes17 vin;
         address owner;
         uint16 year;
         bytes32 make;
@@ -22,21 +22,21 @@ contract Carpool {
         Transmission transmission;
         uint8 seats;
         uint rsvt;
-        Car next; // Link to next car;
+        CarStatus status;
         bool exists;
     }
 
     struct Account {
         address addr;
         bytes32 name;
-        license_t license;
+        bytes16 license;
         bool exists;
     } 
 
     struct Reservation {
         uint num;
         address customer;
-        carid_t carId;
+        uint32 carId;
         uint start;
         uint end;
         bool exists;
@@ -45,27 +45,26 @@ contract Carpool {
     // State variables
     mapping (address => Account) drivers;
     mapping (address => Account) users;
-    mapping (vin_t => Car) cars;
-    mapping (carid_t => Car) idToCar;
+    mapping (bytes17 => Car) cars;
+    mapping (uint32 => Car) idToCar;
     mapping (uint => Reservation) reservations;
 
-    carid_t curCarid;
+    uint32 curCarId;
     uint curRsvtNum;
     
     // Carpool events 
     event DriverRegistered(address account, bytes32 name);
-    event CarRegistered(address account, vin_t vin);
+    event CarRegistered(address account, bytes17 vin);
     event UserRegistered(address account, bytes32 name);
-    event CarReserved(carid_t carId, address customer);
+    event CarReserved(uint32 carId, address customer);
 
-    /// @contructor
     function Carpool() {
         curCarId = 0;
         curRsvtNum = 0;
     }
     
     /// Regster as a driver.
-    function registerDriver(bytes32 name, license_t license) returns(uint error) {
+    function registerDriver(bytes32 name, bytes16 license) returns(uint error) {
         Account driver = drivers[msg.sender];
         if (driver.exists) {
            return ACCOUNT_ALREADY_EXIST;
@@ -73,7 +72,7 @@ contract Carpool {
 
         driver.addr = msg.sender;
         driver.name = name;
-        driver.licence = licence;
+        driver.license = license;
         driver.exists = true;
         drivers[msg.sender] = driver;
 
@@ -82,8 +81,7 @@ contract Carpool {
     }
   
     /// Register a car.
-    function registerACar(address driver,
-        vin_t vin,
+    function registerACar(bytes17 vin,
         uint16 year, 
         bytes32 make, 
         bytes32 model, 
@@ -102,8 +100,8 @@ contract Carpool {
             return CAR_ALREADY_REGISTERED;
         }
         
-        car.id = curCarid;
-        curCarid += 1;
+        car.id = curCarId;
+        curCarId += 1;
         car.vin = vin;
         car.owner = driver.addr;
         car.year = year;
@@ -115,7 +113,7 @@ contract Carpool {
         car.status = CarStatus.AVAILABLE;
         car.exists = true;
 
-        cars[msg.sender] = car; 
+        cars[vin] = car; 
         idToCar[car.id] = car;
 
         CarRegistered(msg.sender, vin);
@@ -142,7 +140,7 @@ contract Carpool {
     function reserve(uint32 carId, 
         uint8 numOfSeats,
         uint start,
-        uint end,
+        uint end
     ) 
         returns (uint error, 
         uint reservationNum
@@ -151,7 +149,7 @@ contract Carpool {
         Account user = users[msg.sender];
         if (!user.exists) return (ACCOUNT_NOT_EXIST, 0);
 
-        Car car = idToCar(carId);
+        Car car = idToCar[carId];
         if (!car.exists) return (CAR_NOT_REGISTERED, 0);
 
         if (car.status == CarStatus.UNAVAILABLE) return (CAR_NOT_AVAILABLE, 0);
@@ -163,11 +161,11 @@ contract Carpool {
         rsvt.carId = carId;
         rsvt.start = start;
         rsvt.end = end;
-        rsvt.exist = true;
+        rsvt.exists = true;
         reservations[rsvt.num] = rsvt;
         car.rsvt = rsvt.num;
 
-        CarReserved(carid, user.addr);
+        CarReserved(carId, user.addr);
         return (SUCCESS, rsvt.num);
     }
     
@@ -188,14 +186,14 @@ contract Carpool {
         Car car = idToCar[carId];
         Reservation rsvt = reservations[car.rsvt];
         if (rsvt.customer != user.addr) {
-            return (ACCOUNT_NOT_AUTORIZE_ACCESS_CAR, 0);
+            return (ACCOUNT_NOT_AUTHORIZE_ACCESS_CAR, 0);
         }
         // TODO: generate token for car access.
         UnlockAuthorized(carId, user.addr);
         return (SUCCESS, 0);
     }
 
-    function requestAuth2Lock(uint32 carId) returns (uint token) {
+    function requestAuth2Lock(uint32 carId) returns (uint error, uint token) {
         Account user = users[msg.sender];
         if (!user.exists) {
             return (ACCOUNT_NOT_EXIST, 0);
@@ -204,7 +202,7 @@ contract Carpool {
         Car car = idToCar[carId];
         Reservation rsvt = reservations[car.rsvt];
         if (rsvt.customer != user.addr) {
-            return (ACCOUNT_NOT_AUTORIZE_ACCESS_CAR, 0);
+            return (ACCOUNT_NOT_AUTHORIZE_ACCESS_CAR, 0);
         }
         // TODO: generate token for car access.
         LockAuthorized(carId, user.addr);
--- contracts/CarpoolTypes.sol
+++ contracts/CarpoolTypes.sol
@@ -1,17 +1,19 @@
-pragma solidity ^0.4.10
+pragma solidity ^0.4.8;
 
-// Typedefs
-using vin_t = bytes17;
-using license_t = bytes16;
-using carid_t = uint32;
+contract CarpoolTypes {
+    //typedef vin_t bytes17; 
+    //typedef license_t bytes16;
+    //typedef carid_t uint32;
+    // Related to solidity issure: https://github.com/ethereum/solidity/issues/1100
 
-// Error codes
-uint constant SUCCESS = 0;
-uint constant ACCOUNT_ALREADY_EXIST = 1;
-uint constant ACCOUNT_NOT_EXIST = 2;
+    // Error codes
+    uint constant SUCCESS = 0;
+    uint constant ACCOUNT_ALREADY_EXIST = 1;
+    uint constant ACCOUNT_NOT_EXIST = 2;
 
-uint constant CAR_ALREADY_REGISTERED = 1000;
-uint constant CAR_NOT_REGISTERED = 1001;
-uint constant CAR_NOT_AVAILABLE = 1002;
-uint constant ACCOUNT_NOT_AUTHORIZE_ACCESS_CAR = 1003;
+    uint constant CAR_ALREADY_REGISTERED = 1000;
+    uint constant CAR_NOT_REGISTERED = 1001;
+    uint constant CAR_NOT_AVAILABLE = 1002;
+    uint constant ACCOUNT_NOT_AUTHORIZE_ACCESS_CAR = 1003;
+}
 
