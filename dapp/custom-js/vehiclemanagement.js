bccd2e53fa26e80283c117f28211e7e2a8952adf
Lexon <co.liang.ol@gmail.com>
2017-05-17 09:31:56 -0400

    revised unlock/lock with signed&dated mqttt protocol

--- dapp/custom-js/vehiclemanagement.js
+++ dapp/custom-js/vehiclemanagement.js
@@ -2,16 +2,48 @@
 const MQTT_BROKER_WS = 'ws://35.166.170.137:3000';
 const TOPIC_CARSHARING = 'carsharing-car-commands-topic'; 
 
+var web3;
 var client = mqtt.connect(MQTT_BROKER_WS);
+
 client.on('connect', () => {
   console.log('connected');
 });
-
+/*
 function lock() {
   client.publish(TOPIC_CARSHARING, "lock");
 }
 
-
 function unlock() {
   client.publish(TOPIC_CARSHARING, "unlock");
+}
+*/
+
+var tClient;
+const CAR_NODE_ADDRESS = "0x3b7f58b6ae8643dac0ac16560b7b38991cdec338";
+
+web3.eth.getAccounts((err, accs) => {
+  if (err) throw err;
+  if (accs.length === 0) throw new Error("No eth accounts available!");
+  var acc = accs[0];
+  console.log('MQTTT is using eth account ' + acc);
+  // TODO: Web3Signer only tested with testrpc, geth may not working with the `eth_sign` 
+  // API change https://github.com/ethereum/go-ethereum/issues/3621
+  tClient = new mqttt.MQTTT(acc, new mqttt.signers.Web3Signer(web3), MQTT_BROKER_WS);
+
+  tClient.listen(true, (err, msg) => {
+    console.log(err, msg);
+    if (err) throw err;
+    var data = JSON.parse(msg.data);
+    if (data.err) throw data.err;
+    alert(data.result);
+  });
+});
+
+
+function lock() {
+  tClient.send(CAR_NODE_ADDRESS, 'lock', 'command');
+}
+
+function unlock() {
+  tClient.send(CAR_NODE_ADDRESS, 'unlock', 'command');
 }
